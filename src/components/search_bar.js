b6ff1545dc872dafa40c714820b4eafd38abb42d
Aviv Varsano <aviv.varsano@gmail.com>
2018-01-02 17:49:07 +0200

    initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+// { Component } - import react and pull of the property component as a variable called component
+
+//const Component = React.Component; //This is equivalent to import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//     return <input /> //We need to import React because this line will be translated to : React.createElement
+// };
+
+//define a new class called SearchBar and give it access to all of the functionality that React.Component has
+//class SearchBar extends React.Component {
+class SearchBar extends Component { // -> equivalent to class SearchBar extends React.Component {
+
+//When we use class based method We still have to give it the ability to render itself somehow (return jsx), to do so we define
+//a method on the class that called render method . every react component that we create that is a class based
+// have to have a render method
+//whenever our app component tries to render SearchBar instead of just calling a normal function it will call the render function
+//every render function must return jsxx
+
+    //This is the way to initialize a state in a class based component, remember functional component do not have state
+    // All JS classes have a special function called Constructor function, the constructor function is the first and only
+    //function that called automatically whenever a new instance of the class is created
+    constructor(props) {
+        super(props); // since we extend Component (that also has a Constructor function)
+
+        this.state = { term: '' }
+    }
+
+    render (){
+        //return <input onChange={this.onInputChange}/>;
+
+
+        /*
+        return <input onChange={(event) => console.log(event.target.value)}/>; //this is an arrow function, the same outcome as the row above
+        return <input onChange={event => console.log(event.target.value)}/>; // we can remove the leading parentecis since we have only 1 argument
+        return <input onChange={event => this.setState({term:event.target.value})}/>;
+        */
+
+
+
+        // return (
+        //     <div>
+        //         <input onChange={event => this.setState({term:event.target.value})}/>
+        //         Value of the input : {this.state.term}
+        //     </div>
+        // )
+
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({term: event.target.value})}/>
+            </div>
+        )
+
+
+        // and also because its a single line we can ommit the carly braclets
+    }
+
+    // onInputChange (event) { // the "event" object describes the context or information about the event that occured
+    //     console.log(event.target.value);
+    // }
+
+
+}
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAUHm4XPCDKBvJijNEF-47nir4QXA84GJo';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+//Create a new component , This component should produce some HTML
+
+// const App = function(){
+//     return <div>Hi!</div>; //JSXX
+// };
+
+// const App = () =>{
+//     return <div>Hi!</div>; //JSXX
+// };
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { videos: []};
+
+        // YTSearch({key:API_KEY, term:'surfboards'}, function(data){
+        //     this.setState = ({videos:data})
+        // });
+        //
+        // YTSearch({key:API_KEY, term:'surfboards'}, function(videos){
+        //     //this.setState = ({videos:videos});
+        //     this.setState = ({videos}) //ES6 convention, same as the above statement just shorter
+        // });
+
+        YTSearch({key:API_KEY, term:'surfboards'}, (videos) =>{
+            //this.setState = ({videos:videos});
+            this.setState = ({videos}) //ES6 convention, same as the above statement just shorter
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )
+    }
+}
+
+// const App = () =>{
+//     return (
+//         <div>
+//             <SearchBar/>
+//         </div>
+//         //<SearchBar/> this is actually a creation of a new "SearchBar"  instance
+//     ); //JSXX
+// };
+//Take this component's generated HTML and put it on the page(in the DOM)
+//ReactDOM.render(<App></App>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
